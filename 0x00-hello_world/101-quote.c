a95e89569b52ead2fd06b68e18b6d54501b1f8e6
Chidera-mmaduka <126536955+Chidera-mmaduka@users.noreply.github.com>
2023-03-17 04:46:53 +0100

    UNIX is basically

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ *main - Entry point
+ *Return: Always 1
  *
- * Return: Always 0 (when success)
  */
+
 int main(void)
 {
-write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-sizeof("and that piece of art is useful\" -  Dora Korpar, 2015-10-19"));    
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
