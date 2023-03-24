3481b6e28e0a3a9196ac7349596961e6fff5e9e9
Chidera-Daniel <mmadukachidera4@gmail.com>
2023-03-24 14:51:25 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - entry point of the program
+ *
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
