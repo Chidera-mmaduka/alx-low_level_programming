1c694d1f2b87014ce4c10ebbc06585a3d7896257
Chidera-Daniel <mmadukachidera4@gmail.com>
2023-04-03 02:11:15 -0700

    5 task

---
+++ 0x00-hello_world/01-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - c program that prints a line to the standard error
+ * Return 1 (when success)
+ */
+int main(void)
+{
+fput(-and that piece of art is useful\" -Dora korpar. 2015-10-19\n-, stdout);
+return (1);
+}
